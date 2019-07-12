9471df7e3ebc901d7ac91bfee1f724884fba9bc0
cwwilson08 <33009556+cwwilson08@users.noreply.github.com>
2019-07-12 10:43:30 -0700

    level bug fix - add leak sensor

--- insteonserver.js
+++ insteonserver.js
@@ -16,7 +16,7 @@ var config = configJSON.platforms[platformIndex]
 
 InsteonServer()
 
-function InsteonServer() {	
+function InsteonServer() {
 	var devices = config.devices
 	var deviceIDs = []
 	var deviceJSON = []
@@ -36,65 +36,65 @@ function InsteonServer() {
 	var pass = config.pass
 	var model = config.model
 	var server_port = config.server_port || 3000
-	
+
 	var hubConfig = {
 		host: host,
 		port: port,
 		user: user,
 		password: pass
 	}
-	
+
 	connectToHub()
 	init()
-	
+
 	app.get('/light/:id/on', function(req, res) {
 		var id = req.params.id.toUpperCase()
 		hub.light(id).turnOn().then(function(status) {
 			if (status.response) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
 		})
 	})
-	
+
 	app.get('/light/:id/off', function(req, res) {
 		var id = req.params.id.toUpperCase()
 		hub.light(id).turnOff().then(function(status) {
 			if (status.response) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
 		})
 	})
-	
+
 	app.get('/light/:id/faston', function(req, res) {
 		var id = req.params.id.toUpperCase()
 		hub.light(id).turnOnFast().then(function(status) {
 			if (status.response) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
 		})
 	})
-	
+
 	app.get('/light/:id/fastoff', function(req, res) {
 		var id = req.params.id.toUpperCase()
 		hub.light(id).turnOffFast().then(function(status) {
 			if (status.response) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
 		})
 	})
-	
+
 	app.get('/light/:id/status', function(req, res) {
 		var id = req.params.id
 		hub.light(id).level(function(err, level) {
@@ -111,7 +111,7 @@ function InsteonServer() {
 		hub.light(id).level(targetLevel).then(function(status) {
 			if (status.response) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
@@ -123,10 +123,10 @@ function InsteonServer() {
 		hub.sceneOn(group).then(function(status) {
 			if (status.aborted) {
 				res.sendStatus(404)
-			} 
+			}
 			if (status.completed) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
@@ -138,10 +138,10 @@ function InsteonServer() {
 		hub.sceneOff(group).then(function(status) {
 			if (status.aborted) {
 				res.sendStatus(404)
-			} 
+			}
 			if (status.completed) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
@@ -173,7 +173,7 @@ function InsteonServer() {
 		hub.ioLinc(id).relayOn().then(function(status) {
 			if (status.response) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
@@ -185,7 +185,7 @@ function InsteonServer() {
 		hub.ioLinc(id).relayOff().then(function(status) {
 			if (status.response) {
 				res.sendStatus(200)
-				
+
 			} else {
 				res.sendStatus(404)
 			}
@@ -205,12 +205,12 @@ function InsteonServer() {
 			res.json(status.relay)
 		})
 	})
-	
+
 	app.listen(server_port)
-	
+
 	function connectToHub() {
 		console.log('Model: ' + model)
-				
+
 		if (model == '2245') {
 			console.log('Connecting to Insteon Model 2245 Hub...')
 			hub.httpClient(hubConfig, function() {
@@ -243,7 +243,7 @@ function InsteonServer() {
 	function init() {
 		console.log('Initiating websocket...')
 		var message
-   
+
 		wss.on('connection', function (ws) {
 			console.log('Client connected to websocket')
 			ws.isAlive = true
@@ -254,7 +254,7 @@ function InsteonServer() {
 			})
 
 			ws.send('Connected to Insteon Server')
-			
+
 			ws.on('message', function (message) {
 				if(message == 'getDevices'){
 					console.log(deviceJSON)
@@ -263,70 +263,89 @@ function InsteonServer() {
 			})
 
 			devices.forEach(function(device){
-				switch (device.deviceType) {     
+				switch (device.deviceType) {
 				case 'doorsensor':
 				case 'windowsensor':
 				case 'contactsensor':
+
+
 					device.door = hub.door(device.deviceID)
-						
-					device.door.on('opened', function(){		
+
+					device.door.on('opened', function(){
 						console.log('Got open for ' + device.name)
 						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: 'open'}
 						if(ws.isAlive){ws.send(JSON.stringify(message))}
 					})
-						
+
 					device.door.on('closed', function(){
 						console.log('Got closed for ' + device.name)
 						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: 'closed'}
 						if(ws.isAlive){ws.send(JSON.stringify(message))}
 					})
-						
+
 					break
 					
-				case 'switch':                  
+					case 'leaksensor':
+					device.leak = hub.leak(device.deviceID)
+
+					device.leak.on('dry', function(){
+						console.log('Got dry for ' + device.name)
+						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: 'dry'}
+						if(ws.isAlive){ws.send(JSON.stringify(message))}
+					})
+
+					device.leak.on('wet', function(){
+						console.log('Got wet for ' + device.name)
+						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: 'wet'}
+						if(ws.isAlive){ws.send(JSON.stringify(message))}
+					})
+
+					break
+
+				case 'switch':
 					device.light = hub.light(device.deviceID)
-						
+
 					device.light.on('turnOn', function (group, level) {
 						console.log(device.name + ' turned on')
-						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: level}  
+						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: level}
 						if(ws.isAlive){ws.send(JSON.stringify(message))}
 					})
 
 					device.light.on('turnOff', function () {
 						console.log(device.name + ' turned off')
-						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: 0}  
+						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: 0}
 						if(ws.isAlive){ws.send(JSON.stringify(message))}
 					})
 
 					break
 
 				case 'lightbulb':
-				case 'dimmer':                   
+				case 'dimmer':
 					device.light = hub.light(device.deviceID)
 					device.light.level().then(function(level) {
-						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: level}  
+						message = {name: device.name, id: device.deviceID, deviceType: device.deviceType, state: level}
 						if(ws.isAlive){ws.send(JSON.stringify(message))}
 					})
 					break
-				}  
+				}
 			})
-			
+
 			eventListener()
 
 			function eventListener() {
 				console.log('Insteon event listener started...')
-				
+
 				hub.on('command', function(data) {
-					
+
 					if (typeof data.standard !== 'undefined') {
 						//console.log('Received command for ' + data.standard.id)
-						
+
 						var info = JSON.stringify(data)
 						var id = data.standard.id.toUpperCase()
 						var command1 = data.standard.command1
 						var command2 = data.standard.command2
 						var messageType = data.standard.messageType
-						
+
 						var isDevice = _.contains(deviceIDs, id, 0)
 						var message
 
@@ -334,53 +353,53 @@ function InsteonServer() {
 							var foundDevices = devices.filter(function(item) {
 								return item.deviceID == id
 							})
-			
+
 							console.log('Found ' + foundDevices.length + ' accessories matching ' + id)
 							console.log('Hub command: ' + info)
-			
+
 							for (var i = 0, len = foundDevices.length; i < len; i++) {
 								var foundDevice = foundDevices[i]
 								console.log('Got event for ' + foundDevice.name + ' (' + foundDevice.deviceID + ')')
-								
+
 								switch (foundDevice.deviceType) {
 								case 'lightbulb':
 								case 'dimmer':
 									if (command1 == '19' || command1 == '03' || command1 == '04' || (command1 == '00' && command2 != '00') || (command1 == '06' && messageType == '1')) { //19 = status
 										var level_int = parseInt(command2, 16) * (100 / 255)
 										var level = Math.ceil(level_int)
-			
+
 										console.log('Got updated status for ' + foundDevice.name)
-										message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: level}  
+										message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: level}
 										if(ws.isAlive){ws.send(JSON.stringify(message))}
 									}
-									
+
 									if (command1 == 11) { //11 = on
 										var level_int = parseInt(command2, 16)*(100/255)
 										var level = Math.ceil(level_int)
-						
+
 										console.log('Got on event for ' + foundDevice.name)
-										message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: 100}  
+										message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: level}
 										if(ws.isAlive){ws.send(JSON.stringify(message))}
 									}
-									
+
 									if (command1 == 12) { //fast on
 										console.log('Got fast on event for ' + foundDevice.name)
-										message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: 100}  
+										message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: 100}
 										if(ws.isAlive){ws.send(JSON.stringify(message))}
 									}
-									
+
 									if (command1 == 13 || command1 == 14) { //13 = off, 14= fast off
 										if (command1 == 13) {
 											console.log('Got off event for ' + foundDevice.name)
 										} else {console.log('Got fast off event for ' + foundDevice.name)}
-										message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: 0}  
-										if(ws.isAlive){ws.send(JSON.stringify(message))}                                      
+										message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: 0}
+										if(ws.isAlive){ws.send(JSON.stringify(message))}
 									}
-									
+
 									if (command1 == 18) { //stop dimming
 										console.log('Got dim event for ' + foundDevice.name)
 										foundDevice.light.level().then(function(level) {
-											message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: level}  
+											message = {name: foundDevice.name, id: foundDevice.deviceID, deviceType: foundDevice.deviceType, state: level}
 											if(ws.isAlive){ws.send(JSON.stringify(message))}
 										})
 									}
@@ -395,3 +414,4 @@ function InsteonServer() {
 		}
 		)}
 }
+
